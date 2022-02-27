6208c07c18ab2b9505f02094af24a8ce4ecf87ee
Erick <erick1junior@hotmail.com>
2022-02-27 13:22:43 -0300

    Update files

--- create-table.sql
+++ create-table.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `relatorio` (
-  `data` date NOT NULL,
-  `material` varchar(45) NOT NULL,
-  `titulo` varchar(100) NOT NULL,
-  `cliente` varchar(100) NOT NULL,
-  `valor` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `ID` varchar(40) NOT NULL,
+  `Data` date NOT NULL,
+  `Material` varchar(45) NOT NULL,
+  `Titulo` varchar(100) NOT NULL,
+  `Cliente` varchar(100) NOT NULL,
+  `Valor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 SELECT * FROM `handspike-media`.relatorio;
--- index.php
+++ index.php
@@ -2,12 +2,13 @@
     if(isset($_POST['submit']))
     {
         include_once('config.php');
+        $id = $_POST['id'];
         $dataa = $_POST['data'];
         $material = $_POST['material'];
         $titulo = $_POST['titulo'];
         $cliente = $_POST['clientes'];
         $valor = $_POST['valor'];
-        $result = mysqli_query($conexao, "INSERT INTO relatorio(data,material,titulo,cliente,valor) VALUES ('$dataa', '$material', '$titulo','$cliente','$valor')");
+        $result = mysqli_query($conexao, "INSERT INTO relatorio(ID,Data,Material,Titulo,Cliente,Valor) VALUES ('$id','$dataa', '$material', '$titulo','$cliente','$valor')");
     }
 ?>
 
@@ -23,9 +24,15 @@
 </head>
 <body>
     <main class="container">
-        <h2>Handspike Media <br></h2>
-        <h2>Data de hoje</h2>
+        <h2 class="titulo">Handspike Media <br></h2>
+        <h2>Quem é você?</h2>
         <form action = "index.php" method="POST">
+        <div class="input-field">
+                <label for="radio-erick"><input required type="radio" name="id" value="Erick" id="radio-erick">Erick</label>
+                <label for="radio-felipe"><input required type="radio" name="id" value="Felipe" id="radio-felipe">Felipe</label>
+                <div class="underline"></div>
+            </div>
+        <h2>Data de hoje</h2>
             <div class="input-field">
                 <input required type="date" name="data" id="input-data"
                     placeholder="Insira a data de hoje">
@@ -33,9 +40,9 @@
             </div>
             <h2>Material</h2>
             <div class="input-field">
-                <label for="radio-arte"><input required type="radio" name="material" value="arte" id="radio-arte">Arte</label>
-                <label for="radio-video"><input required type="radio" name="material" value="video" id="radio-video">Vídeo</label>
-                <label for="radio-outro"><input required type="radio" name="material" value="outro" id="radio-outro">Outro</label>
+                <label for="radio-arte"><input required type="radio" name="material" value="Arte" id="radio-arte">Arte</label>
+                <label for="radio-video"><input required type="radio" name="material" value="Video" id="radio-video">Vídeo</label>
+                <label for="radio-outro"><input required type="radio" name="material" value="Outro" id="radio-outro">Outro</label>
                 <div class="underline"></div>
             </div>
             <h2>Título</h2>
@@ -47,20 +54,20 @@
             <h2>Cliente</h2>
                 <div class="input-field"></div>
                     <select name="clientes" id="input-cliente">
-                    <option value="carrossel">Carrossel</option>
-                    <option value="criancaTodoDia">Criança Todo Dia</option>
-                    <option value="designDecoracao">Design & Decoração</option>
-                    <option value="diversaoNautica">Diversão Náutica</option>
-                    <option value="gasLindaChama">Gás Linda Chama</option>
-                    <option value="handspike">Handpike Media</option>
-                    <option value="esplanada">Hotel Esplanada</option>
-                    <option value="jfCred">JF Cred</option>
-                    <option value="lares">Lares Materiais de Construção</option>
-                    <option value="maisCelulares">Mais Celulares</option>
-                    <option value="oticasCarol">Óticas Carol</option>
-                    <option value="sportTotal">Sport Total</option>
-                    <option value="zrImoveis">ZR Imóveis</option>
-                    <option value="outro">Outro</option>
+                    <option value="Carrossel">Carrossel</option>
+                    <option value="CriancaTodoDia">Criança Todo Dia</option>
+                    <option value="Design&Decoracao">Design & Decoração</option>
+                    <option value="DiversaoNautica">Diversão Náutica</option>
+                    <option value="GasLindaChama">Gás Linda Chama</option>
+                    <option value="Handspike">Handpike Media</option>
+                    <option value="EsplanadaHotel">Hotel Esplanada</option>
+                    <option value="JFCred">JF Cred</option>
+                    <option value="Lares">Lares Materiais de Construção</option>
+                    <option value="MaisCelulares">Mais Celulares</option>
+                    <option value="OticasCarol">Óticas Carol</option>
+                    <option value="SportTotal">Sport Total</option>
+                    <option value="ZRImoveis">ZR Imóveis</option>
+                    <option value="Outro">Outro</option>
                     </select>
                 </div>
             <h2>Valor</h2>
